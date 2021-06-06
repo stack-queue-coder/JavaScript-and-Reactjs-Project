aa7be0fa55d78184243649bc51ccb9fe425d8883
stack-queue-coder <60400251+stack-queue-coder@users.noreply.github.com>
2021-06-06 13:32:27 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amzaing bootcamp taken up by Shaurya Sinha sir We covered
+        everything from scratch including javascript , React.js
+      </p>
+    </div>
+  );
+}
+
+export default Note;
